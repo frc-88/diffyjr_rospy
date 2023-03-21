0199ca094248c63c6656ce0125bed694ce00b152
Woz4tetra <woz4tetra@gmail.com>
2023-03-21 12:51:03 -0400

    rospy and wpilib playing nice

--- src/robot.py
+++ src/robot.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
 
+import os
+import importlib
+
+HOST_MACHINE = "10.0.88.44"
+os.environ["ROS_IP"] = "10.0.88.2"
+os.environ["ROS_MASTER_URI"] = f"http://{HOST_MACHINE}:11311"
+os.environ["ROS_PYTHON_LOG_CONFIG_FILE"] = "/home/lvuser/py/python_logging.yaml"
+
+import logging
 import rospy
 import wpilib
-import wpilib.drive
+from std_msgs.msg import Int64
+importlib.reload(logging)
 
 
 class MyRobot(wpilib.TimedRobot):
-
     def robotInit(self):
         """
         This function is called upon program startup and
         should be used for any initialization code.
         """
+        print("Initializing node")
         self.timer = wpilib.Timer()
+        rospy.init_node(
+            "diffyjr_roborio"
+            # disable_signals=True
+            # log_level=rospy.DEBUG
+        )
+        self.count = 0
+        self.talker = rospy.Publisher("talker", Int64, queue_size=10)
+        print("Robot initialized!")
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
@@ -22,6 +40,11 @@ class MyRobot(wpilib.TimedRobot):
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
+    
+    def disabledPeriodic(self):
+        self.count += 1
+        self.talker.publish(Int64(self.count))
+        rospy.loginfo(f"count: {self.count}")
 
 
 if __name__ == "__main__":
